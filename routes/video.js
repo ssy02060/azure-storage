ee4316089ff02daf74c7a0c5cc409c48de30c82a
ssy02060 <ssy02060@gmail.com>
2022-07-21 00:12:53 +0900

    add env check code

--- routes/video.js
+++ routes/video.js
@@ -3,9 +3,12 @@ var azure = require('azure-storage');
 
 var router = express.Router();
 
-const STORAGE_ACCOUNT_NAME  = process.env.STORAGE_ACCOUNT_NAME;
-const STORAGE_ACCESS_KEY    = process.env.STORAGE_ACCESS_KEY;
+const STORAGE_ACCOUNT_NAME = process.env.STORAGE_ACCOUNT_NAME;
+const STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;
 
+if (!process.env.PORT) {
+  throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
 
 if (!process.env.STORAGE_ACCOUNT_NAME) {
   throw new Error("Please specify the name of an Azure storage account in environment variable STORAGE_ACCOUNT_NAME.");
@@ -30,37 +33,37 @@ router.get("/", (req, res) => {
 
   const videoPath = req.query.path;
   console.log(`Streaming video from path ${videoPath}.`);
-  
+
   const blobService = createBlobService();
 
   const containerName = "videos";
   blobService.getBlobProperties(containerName, videoPath, (err, properties) => { // Sends a HTTP HEAD request to retreive video size.
+    if (err) {
+      console.error(`Error occurred getting properties for video ${containerName}/${videoPath}.`);
+      console.error(err && err.stack || err);
+      res.sendStatus(500);
+      return;
+    }
+
+    //
+    // Writes HTTP headers to the response.
+    //
+    res.writeHead(200, {
+      "Content-Length": properties.contentLength,
+      "Content-Type": "video/mp4",
+    });
+
+    //
+    // Streams the video from Azure storage to the response.
+    //
+    blobService.getBlobToStream(containerName, videoPath, res, err => {
       if (err) {
-          console.error(`Error occurred getting properties for video ${containerName}/${videoPath}.`);
-          console.error(err && err.stack || err);
-          res.sendStatus(500);
-          return;
+        console.error(`Error occurred getting video ${containerName}/${videoPath} to stream.`);
+        console.error(err && err.stack || err);
+        res.sendStatus(500);
+        return;
       }
-
-      //
-      // Writes HTTP headers to the response.
-      //
-      res.writeHead(200, {
-          "Content-Length": properties.contentLength,
-          "Content-Type": "video/mp4",
-      });
-
-      //
-      // Streams the video from Azure storage to the response.
-      //
-      blobService.getBlobToStream(containerName, videoPath, res, err => {
-          if (err) {
-              console.error(`Error occurred getting video ${containerName}/${videoPath} to stream.`);
-              console.error(err && err.stack || err);
-              res.sendStatus(500);
-              return;
-          }
-      });
+    });
   });
 });
 
